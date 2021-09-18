9c4ac8b558d092c915685465fce022798d2b220e
alan-T <807733783@qq.com>
2021-09-18 17:03:08 +0800

    测试link

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
 
 const Home = (props) => {
     return (
         <div>
             Home
+            <Link to="/news">News</Link>
         </div >
     )
 }

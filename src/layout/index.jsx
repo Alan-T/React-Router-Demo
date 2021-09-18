f600dff0ea2da24bbfd2726e032548c770c81d80
alan-T <807733783@qq.com>
2021-09-18 15:50:25 +0800

    添加路由

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
-import Home from '../pages/home';
-import User from '../pages/user';
-import News from '../pages/news';
-
-const BaseLayout = () => {
+const BaseLayout = (props) => {
     return <div>
         <BrowserRouter>
             <Link to="/">Home</Link>
             <Link to="/user">User</Link>
             <Link to="/news">News</Link>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/user" component={User} />
-                <Route exact path="/news" component={News} />
-            </Switch>
+            <Suspense fallback={<div>正在加载...</div>}>
+                <Switch>
+                    <Route exact path="/" component={React.lazy(() => import(`../pages/home`))} />
+                    <Route exact path="/user" component={React.lazy(() => import(`../pages/user`))} />
+                    <Route exact path="/news" component={React.lazy(() => import(`../pages/news`))} />
+                </Switch>
+            </Suspense>
         </BrowserRouter>
     </div>
 }
--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const News = () => {
+const News = (props) => {
+    debugger
     return (
         <div>
             News

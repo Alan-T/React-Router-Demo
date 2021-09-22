bb90192614281cbb2c8bc892cbc2a717acf6e0db
alan-T <807733783@qq.com>
2021-09-22 15:52:41 +0800

    路由跳转完成

---
+++ src/layouts/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './footer.module.less';
+
+const Footer = () => {
+    return <footer>
+        <section>
+            页脚
+        </section>
+    </footer>
+
+}
+export default Footer
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,23 +1,17 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
 
-const BaseLayout = (props) => {
-    debugger
+const Layout = (props) => {
     return <div>
         <BrowserRouter>
-            <Link to="/">Home</Link>
-            <Link to="/news">News</Link>
-            <Link to="/setting">Setting</Link>
             <Suspense fallback={<div>正在加载...</div>}>
                 <Switch>
-                    {/* <Redirect to="/home" /> */}
-                    <Route exact path="/" component={React.lazy(() => import(`../pages/home`))} />
-                    <Route exact path="/news" component={React.lazy(() => import(`../pages/news`))} />
-                    <Route exact path="/setting" component={React.lazy(() => import(`../pages/setting`))} />
+                    <Route exact path="/login" component={React.lazy(() => import(`../pages/login`))} />
+                    <Route path="/" component={React.lazy(() => import(`../layouts/base-layout`))} />
                 </Switch>
             </Suspense>
         </BrowserRouter>
     </div>
 }
 
-export default BaseLayout;
+export default Layout;

b8f014bb554fbc4e32168ef92da03b105f10bd3f
Alan-T <807733783@qq.com>
2021-09-24 00:08:32 +0800

    微调

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,16 +1,14 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
+import { Route, Switch, Link, Redirect } from 'react-router-dom';
 
 const Layout = (props) => {
     return <div>
-        <BrowserRouter>
-            <Suspense fallback={<div>正在加载...</div>}>
-                <Switch>
-                    <Route exact path="/login" component={React.lazy(() => import(`../pages/login`))} />
-                    <Route path="/" component={React.lazy(() => import(`../layouts/base-layout`))} />
-                </Switch>
-            </Suspense>
-        </BrowserRouter>
+        <Suspense fallback={<div>正在加载...</div>}>
+            <Switch>
+                <Route exact path="/login" component={React.lazy(() => import(`../pages/login`))} />
+                <Route path="/" component={React.lazy(() => import(`../layouts/base-layout`))} />
+            </Switch>
+        </Suspense>
     </div>
 }
 
